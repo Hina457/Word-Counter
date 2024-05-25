8d95669dac6ddc7a9799497b37a3662f21fab57d
Hina Alvi <princess4177812@gmail.com>
2024-05-25 02:08:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+//import the 'inquirer' which is a cammand line intertface.
+import inquirer from "inquirer";
+//Declare a constant 'answer' and assign it to the result of inquirer.prompt function.
+console.log("Wel come to Hina alvi word counter project");
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// print the array of words to the console.
+console.log(words);
+// print the word count of the sentence to the console.
+console.log(`your sentence word count is ${words.length}`);
+console.log("Thank you!");
